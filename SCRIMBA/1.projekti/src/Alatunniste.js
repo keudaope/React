bef632d08fc75607313aca114eb40c23ce2b217c
keudaope <jyri.lindroos@keuda.fi>
2023-10-26 17:57:28 +0300

    react2

---
+++ SCRIMBA/1.projekti/src/Alatunniste.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Alatunniste() {
+    return (<small className="footer">&copy; Jyri Lindroos. Kaikki oikeudet pidätetään</small>)
+}
+
+export default Alatunniste;
+
