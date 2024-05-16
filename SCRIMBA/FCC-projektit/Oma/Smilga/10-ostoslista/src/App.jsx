dd422c1ebd6d893e835d9cd1fcc8e13e50641219
Jyri Lindroos <59079476+keudaope@users.noreply.github.com>
2024-05-16 09:24:18 +0300

    10-tehtävän loppu

--- SCRIMBA/FCC-projektit/Oma/Smilga/10-ostoslista/src/App.jsx
+++ SCRIMBA/FCC-projektit/Oma/Smilga/10-ostoslista/src/App.jsx
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -20,10 +28,18 @@ function App() {
       // display alert
       showAlert(true, "danger", "Please enter value");
     } else if (name && isEditing) {
-      setList(list.map((item) => {
-        if(item.id === editID)
-        return item
-      }))
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
     } else {
       showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
@@ -44,15 +60,18 @@ function App() {
     setList(list.filter((item) => item.id !== id));
   };
   const editItem = (id) => {
-    const specificItem = list.find((item) => item.id === id)
-    setIsEditing(true)
-    setEditID(id)
-    setName(specificItem.title)
-  }
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
