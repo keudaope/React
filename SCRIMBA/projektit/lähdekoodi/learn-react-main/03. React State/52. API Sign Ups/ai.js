a87532e7b584b7a473c451a3e7e38a9d4daff9bb
Jyri Lindroos <59079476+keudaope@users.noreply.github.com>
2025-02-23 12:30:33 +0200

    ei saatu APIa toimimaan, tehtiin toisella tavalla kuin videossa

--- SCRIMBA/projektit/lähdekoodi/learn-react-main/03. React State/52. API Sign Ups/ai.js
+++ SCRIMBA/projektit/lähdekoodi/learn-react-main/03. React State/52. API Sign Ups/ai.js
@@ -1,12 +1,12 @@
 import { HFInference } from "@huggingface/inference";
 
-const SYSTEM__PROMPT = ``
+const SYSTEM__PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they
 could make with some or all of those ingredients. You don't need to use every ingredient they
 mention in your recipe. The recipe can include additional ingredients they didn't mention, but try
 not to include too many extra ingredients. Format you response in markdown to make it easier to render to a web page
 `;
-const hf = new HFInference({ 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 });
+const hf = new HFInference({});
 
 export async function getRecipeFromMistral(ingredientArr) {
   const ingredientsString = ingredientArr.join(", ");
